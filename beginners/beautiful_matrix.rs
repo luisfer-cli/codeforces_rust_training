6dd84f04ab4111e7fdb6b291e3db2de03cce26a5
Luis Fernando Rodríguez Vargas <luis.rodriguez21@ucr.ac.cr>
2026-01-10 13:29:04 -0600

    Solved beautiful_matrix exercise

--- beginners/beautiful_matrix.rs
+++ beginners/beautiful_matrix.rs
@@ -10,7 +10,7 @@ transformations to the matrix:
 
 Swap two neighboring matrix rows, that is, rows with indexes i and i + 1 for some integer i
 (1 ≤ i < 5). Swap two neighboring matrix columns, that is, columns with indexes j and j + 1 for
-some integer j (1 ≤ j < 5). 
+some integer j (1 ≤ j < 5).
 
 You think that a matrix looks beautiful, if the single number one of the matrix is located in its
 middle (in the cell that is on the intersection of the third row and the third column). Count the
@@ -23,3 +23,32 @@ number one. Output
 
 Print a single integer — the minimum number of moves needed to make the matrix beautiful.
 **/
+use std::io::{self, Read};
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    let mut lines = input.lines();
+    let mut x: i8 = 0;
+    let mut y: i8 = 0;
+    let mut finish: bool = false;
+    for row in 1..6 {
+        let mut current = lines.next().unwrap().split_whitespace();
+        for column in 1..6 {
+            let element: i8 = current.next().unwrap().parse().unwrap();
+            if element == 1 {
+                x = row;
+                y = column;
+                finish = true;
+                break;
+            }
+        }
+        if finish {
+            break;
+        }
+    }
+    let resultx = (x - 3).abs();
+    let resulty = (y - 3).abs();
+    let result = resulty + resultx;
+    println!("{}", result);
+}
