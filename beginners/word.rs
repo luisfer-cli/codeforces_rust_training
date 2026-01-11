e91bb7ef83487a1e11cd0a7ed67f416cb1f640a2
Luis Fernando Rodríguez Vargas <luis.rodriguez21@ucr.ac.cr>
2026-01-10 22:54:00 -0600

    Solve word exercise

---
+++ beginners/word.rs
@@ -0,0 +1,38 @@
+/**
+A. Word time limit per test
+2 seconds memory limit per test
+256 megabytes
+
+Vasya is very upset that many people on the Net mix uppercase and lowercase letters in one word.
+That's why he decided to invent an extension for his favorite browser that would change the
+letters' register in every word so that it either only consisted of lowercase letters or, vice
+versa, only of uppercase ones. At that as little as possible letters should be changed in the word.
+For example, the word HoUse must be replaced with house, and the word ViP — with VIP. If a word
+contains an equal number of uppercase and lowercase letters, you should replace all the letters
+with lowercase ones. For example, maTRIx should be replaced by matrix. Your task is to use the
+given method on one given word. Input
+
+The first line contains a word s — it consists of uppercase and lowercase Latin letters and
+possesses the length from 1 to 100. Output
+
+Print the corrected word s. If the given word s has strictly more uppercase letters, make the word
+written in the uppercase register, otherwise - in the lowercase one.
+**/
+use std::io::{self, Read};
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    let word = input.lines().next().unwrap();
+    let mut count_cap = 0;
+    for c in word.chars() {
+        if c.is_uppercase() {
+            count_cap = count_cap + 1;
+        }
+    }
+    if count_cap > word.len() / 2 {
+        println!("{}", word.to_uppercase())
+    } else {
+        println!("{}", word.to_lowercase())
+    }
+}
